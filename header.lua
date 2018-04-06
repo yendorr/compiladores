1320e7d7215c82cd62236c868ce18e377dbf73d0
yendorr <ro.rss.129@gmail.com>
2018-04-06 00:00:02 -0300

    fazendo o automato

---
+++ header.lua
@@ -0,0 +1,118 @@
+function isAlpha(char)
+  asc = string.byte(char)
+  return (asc >= 65 and asc <=90) or (asc>=97 and asc<=122)
+end
+
+function isBin(char)
+  asc = string.byte(char)
+  return (asc == 48 or asc == 49)
+end
+    
+function isOcta(char)
+  asc = string.byte(char)
+  return (asc >= 48 and asc<=55)
+end
+
+function isDec(char)  
+    asc = string.byte(char)
+    return (asc >= 48 and asc <= 57)
+end
+
+function isHexa(char)
+    asc = string.byte(char)
+    return (asc >= 48 and asc <= 57) or (asc >= 65 and asc <= 70) or (asc >= 97 and asc <= 102)
+end
+
+function isAritmetico(char)
+    asc = string.byte(char)
+    return asc == 42 or asc == 43 or asc == 47
+end
+
+function isMenos(char)
+    asc = string.byte(char)
+    return asc == 45
+end
+
+function isMaior(char)
+    asc = string.byte(char)
+    return asc == 62
+end
+
+function isMenor(char)
+    asc = string.byte(char)
+    return asc == 60
+end
+
+function isIgual(char)
+    asc = string.byte(char)
+    return asc == 61
+end
+
+function isDP(char)
+    asc = string.byte(char)
+    return asc == 58
+end
+
+function isPV(char)
+    asc = string.byte(char)
+    return asc == 59
+end
+
+function isPonto(char)
+    asc = string.byte(char)
+    return asc == 46
+end
+
+function isVirgula(char)
+    asc = string.byte(char)
+    return asc == 44
+end  
+
+function isSifrao(char)
+    asc = string.byte(char)
+    return asc == 36
+end
+
+function isPorcento(char)
+    asc = string.byte(char)
+    return asc == 37
+end
+
+function isAspas(char)
+    asc = string.byte(char) 
+    return asc == 34
+end
+
+function isAChave(char)
+    asc = string.byte(char)
+    return asc == 123
+end
+
+function isAParente(char)
+    asc = string.byte(char)
+    return asc == 40
+end
+
+function isBparente(char)
+    asc = string.byte(char)
+    return asc == 41
+end
+
+function isBchave(char)
+    asc = string.byte(char)
+    return asc == 125
+end
+
+function isComercial(char)
+    asc = string.byte(char)
+    return asc == 38
+end
+
+function isUnderLine(char)
+    asc = string.byte(char)
+    return asc == 95
+end
+
+function colado(char)
+  return isAritmetico(char) or isMenos(char) or isMenor(char) or isMaior(char) or isIgual(char) or isPV(char)       or isDP(char) or isVirgula(char)
+end  
--- masoq.lua
+++ masoq.lua
@@ -1,16 +1,201 @@
+require "header"
 linhas = 0
-function  getToken(line,linhas)
+a = {}
+function getTokens(line,linhas)
   for word in string.gmatch(line, '[^ \n]+') do
-    print(word);
+    for i = 1, #word do
+      local c = word:sub(i,i+1)
+      --a[linhas][i] = c;
+      --print(a[linhas][i])
+    end
+    --print(word)
+    local i = 1
+    local len = word:len()
+    
+    while i<=len do
+      c = word:sub(i,i)
+      token = c
+      --ai meu deus
+      if isMenor(c) then
+          i=i+1
+          c = word:sub(i,i)
+          if isMaior(c) and i<=len then
+              token = token .. c
+              --salva operador diferente
+              print("diferente",token)
+          elseif isIgual(c) and i<=len then
+              token = token .. c
+              --salva operador menor igual
+              print("igual", token)
+          else
+              i=i-1
+              c = word:sub(i,i)
+              print("menor", token)
+          end
+      elseif  isMaior(c) then
+          i=i+1
+          c = word:sub(i,i)
+          
+          if isIgual(c) and i<=len then
+              token = token .. c
+              --salva operador maior igual
+              print("maior igual",token)
+          else
+              i=i-1
+              c = word:sub(i,i)
+              --salva operador maior 
+              print("maior",token)
+          end
+      
+      elseif  isIgual(c) then
+          i=i+1
+          c = word:sub(i,i)
+          
+          if isIgual(c) and i<=len then
+              token = token .. c
+              --salva operador igualdade
+              print("igual",token)
+          else
+              i=i-1
+          end
+      elseif  isComercial(c) then
+          i=i+1
+          c = word:sub(i,i)
+          
+      
+      elseif  isPorcento(c) then
+          i=i+1
+          c = word:sub(i,i)
+          while(i<=len and isBin(c)) do
+              token = token .. c
+              i=i+1
+              c = word:sub(i,i)
+          end
+          if(i<=len and not isBin(c)) then
+              token = token .. c
+              i=len+1
+              --salva token com marca de erro
+              print("binario com mais de dois simbolos",token)
+          else
+              --salva numero binario
+              print("binario",token)
+          end
+          
+      elseif  isSifrao(c) then
+          i=i+1
+          c = word:sub(i,i)
+          while(i<=len and isHexa(c)) do
+              token = token .. c
+              i=i+1
+              c = word:sub(i,i)
+          end 
+          if(i<=len and not isHexa(c)) then
+              token = token .. c
+              i=len+1
+              --salva token com marca de erro
+              print("Hexa com mais de 16 simbolos",token)
+          else
+              --salva numero hexa
+              print("Hexa",token)
+          end
+      
+      elseif  isDP(c) then
+          i=i+1
+          c = word:sub(i,i)
+          if(i<=len and isIgual(c)) then
+              token = token .. c
+              --salva operador atribuição
+              print("atribuição", token)
+          else
+              i=i-1
+              --salva ::::
+              print("dois pontos", token)
+          end  
+      
+      elseif  isMenos(c) then
+          --salva menos
+          print("menos",token)
+      
+      elseif  isAritmetico(c) then
+          --salva operador
+          printf("operador",token)
+          
+      elseif isAlpha(c) then
+        --i=i+1
+        --c = word:sub(i,i)
+        --while( )do
+          
+        --end
+      
+      elseif isAspas(c) then
+      
+      elseif isAChave(c) then
+      
+      elseif isAParente(c) then
+      
+      elseif isAChave(c) then
+      
+      elseif isDec(c) then
+          i=i+1
+          c = word:sub(i,i)
+          while(i<=len and isDec(c)) do
+              token = token .. c
+              i=i+1
+              c = word:sub(i,i)
+          end
+          if(i<=len and not isDec(c)) then
+            if colado(c) then
+            -- salva numero
+            print("numero1",token)
+            i=i-1
+            elseif (isPonto(c)) then
+                token = token .. c
+                i=i+1
+                c = word:sub(i,i)
+                while(i<=len and isDec(c)) do
+                    token = token .. c
+                    i=i+1
+                    c = word:sub(i,i)
+                end
+                if(i<=len and not isDec(c)) then
+                  if colado(c) then
+                  -- salva numero
+                  print("numero2",token)
+                  i=i-1
+                  else
+                      --salva erro no numero
+                    token = token .. c
+                    print("não possui apenas numeros1", token)
+                    i=len+1
+                  --else
+                    --print("float",token)
+                    --salva numero com numero decimal
+                  end
+                end
+            else
+                --salva erro no numero
+                token = token .. c
+                print("não possui apenas numeros2", token)
+                i=len+1
+            end
+          end
+      end
+      i=i+1
+    end
+    
   end
 end
 
 file = 'pascal.txt'
 f = io.open(file, "rb")
-  if f == nil then print("deu ruim") else print("deu bom") end
+  if f == nil then 
+    print("deu ruim") 
+  end
 
 for line in io.lines(file) do 
   linhas = linhas + 1; 
-  getToken(line,linhas)
-end
+  getTokens(line,linhas)
+  a[linhas] = {};
 
+end
+  
