12e16e90c3d23d2b9b3e25a35dda2cfa0d943925
yendorr <ro.rss.129@gmail.com>
2018-05-18 17:10:54 -0300

    lixo

--- masoq.lua
+++ masoq.lua
@@ -1,6 +1,9 @@
 require "header"
 linhas = 0
-a = {}
+
+local tokenm={};
+local contadorToken=0;
+
 function getTokens(line,linhas)
   for word in string.gmatch(line, '[^ \n]+') do
     for i = 1, #word do
@@ -14,7 +17,7 @@ function getTokens(line,linhas)
     
     while i<=len do
       c = word:sub(i,i)
-      token = c
+      local token = c
       --ai meu deus
       if isMenor(c) then
           i=i+1
@@ -22,15 +25,18 @@ function getTokens(line,linhas)
           if isMaior(c) and i<=len then
               token = token .. c
               --salva operador diferente
-              print("diferente",token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"diferente",token)
           elseif isIgual(c) and i<=len then
               token = token .. c
               --salva operador menor igual
-              print("Menor igual", token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"Menor igual", token)
           else
               i=i-1
               c = word:sub(i,i)
-              print("menor", token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"menor", token)
           end
       elseif  isMaior(c) then
           i=i+1
@@ -39,12 +45,14 @@ function getTokens(line,linhas)
           if isIgual(c) and i<=len then
               token = token .. c
               --salva operador maior igual
-              print("maior igual",token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"maior igual",token)
           else
               i=i-1
               c = word:sub(i,i)
               --salva operador maior 
-              print("maior",token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"maior",token)
           end
       
       elseif  isIgual(c) then
@@ -54,7 +62,8 @@ function getTokens(line,linhas)
           if isIgual(c) and i<=len then
               token = token .. c
               --salva operador igualdade
-              print("igual",token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"igual",token)
           else
               i=i-1
           end
@@ -73,10 +82,12 @@ function getTokens(line,linhas)
               token = token .. c
               i=len+1
               --salva token com marca de erro
-              print("binario com mais de dois simbolos",token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"binario com mais de dois simbolos",token)
           else
               --salva numero binario
-              print("binario",token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"binario",token)
           end
           
       elseif  isSifrao(c) then
@@ -90,16 +101,19 @@ function getTokens(line,linhas)
           if(i<=len and not isHexa(c)) then
               token = token .. c
               i=len+1
+              contadorToken = contadorToken + 1;
               --salva token com marca de erro
-              print("Hexa com mais de 16 simbolos",token)
+              print(contadorToken,"Hexa com mais de 16 simbolos",token)
           else
+              contadorToken = contadorToken + 1;
               --salva numero hexa
-              print("Hexa",token)
+              print(contadorToken,"Hexa",token)
           end
           
       elseif isPV(c) then
           --salva pronto e virgula
-          print("charles",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"charles",token)
           
       elseif  isDP(c) then
           i=i+1
@@ -107,20 +121,24 @@ function getTokens(line,linhas)
           if(i<=len and isIgual(c)) then
               token = token .. c
               --salva operador atribuição
-              print("atribuição", token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"atribuição", token)
           else
               i=i-1
               --salva ::::
-              print("dois pontos", token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"dois pontos", token)
           end  
       
       elseif  isMenos(c) then
           --salva menos
-          print("menos",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"menos",token)
           
       elseif  isAritmetico(c) then
           --salva operador
-          printf("operador",token)
+          contadorToken = contadorToken + 1;
+          printf(contadorToken,"operador",token)
           
       elseif isAlpha(c) then
           i=i+1
@@ -132,107 +150,139 @@ function getTokens(line,linhas)
           end
           i=i-1
           --verifica e salva palavra
-          print("palavra",token);
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"palavra",token);
       elseif isAspa(c) then
           --salva aspa
-          print("aspa", token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"aspa", token)
       elseif isAspas(c) then
           --salva aspas
-          print("aspas",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"aspas",token)
           
       elseif isAChave(c) then
           --salva abrechave
-          print("Achave",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"Achave",token)
       elseif isAParente(c) then
           --salva abreParenteses
-          print("Aparente",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"Aparente",token)
         
       elseif isAColchete(c) then
           --salva abreColchete
-          print("AColchete",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"AColchete",token)
           
       elseif isBChave(c) then
           --salva fechachave
-          print("Bchave",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"Bchave",token)
           
       elseif isPonto(c) then
           i=i+1
           c = word:sub(i,i)
           if(isPonto(c))then
               -- opa dois pontos
-              print("aqueles dois pontos",token)
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"aqueles dois pontos",token)
           else
               -- tem?
+              contadorToken = contadorToken + 1;
               i=i-1
-              print("um ponto",token)
+              print(contadorToken,"um ponto",token)
           end
           
       elseif isBParente(c) then
           --salva fechaParenteses
-          print("Bparente",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"Bparente",token)
           
       elseif isBColchete(c) then
           --salva fechaColchete
-          print("BColchete",token)
+          contadorToken = contadorToken + 1;
+          print(contadorToken,"BColchete",token)
           
       elseif isDec(c) then
           i=i+1
           c = word:sub(i,i)
+          print(len,word);
           local hmm = true
           while(i<=len and isDec(c)) do
               token = token .. c
               i=i+1
               c = word:sub(i,i)
           end
+          --ok
+          print("opa", i , c);
           if(i<=len and not isDec(c)) then
-            if colado(c) then
-            -- ou n salva numero
-            print("numero1",token)
-            i=i-1
-            elseif (isPonto(c)) then
-                token = token .. c
-                i=i+1
-                c = word:sub(i,i)
-                while(i<=len and isDec(c)) do
-                    token = token .. c
-                    i=i+1
-                    c = word:sub(i,i)
-                end
-                if(i<=len and not isDec(c)) then
-                  if colado(c) then
-                  -- salva numero
-                  print("numero2",token)
+              if colado(c) then
+                  -- ou n salva numero
+                  contadorToken = contadorToken + 1;
+                  print(contadorToken,"numero1",token)
                   i=i-1
-                  --elseif () 
+              elseif (isPonto(c)) then
+                  local d = word:sub(i+1,i+1);
+                  if(isPonto(d))  then
+                      contadorToken = contadorToken +1;
+                      print(contadorToken,"numero ant",token);
+                      token = "..";
+                      contadorToken = contadorToken +1;
+                      print(contadorToken,"numero3",token);
+                      hmm = false;
                   else
-
-                      --salva erro no numero
-                    token = token .. c
-                    hmm = false
-                    --print("não possui apenas numeros1", token)
-                    i=len+1
-                  --else
-                    print("float1",token)
-                    --salva numero com numero decimal
+                      hmm = true;
                   end
-                else --i>len
+              else
+                  token = token .. c
+                  i=i+1
+                  c = word:sub(i,i)
+                  while(i<=len and isDec(c)) do
+                      token = token .. c
+                      i=i+1
+                      c = word:sub(i,i)
+                  end
+              end
+              if(i<=len and not isDec(c)) then
+                  if colado(c) then
+                      -- salva numero
+                      contadorToken = contadorToken +1;
+                      print(contadorToken,"numero2",token);
+                      i=i-1;
+                      --elseif ()
+                      --salva erro no numero
+                      token = token .. c
+                      hmm = false
+                      --print("não possui apenas numeros1", token)
+                      i=len+1
+                  else
+                      contadorToken = contadorToken + 1;
+                      print(contadorToken,"float1",token)
+                      --salva numero com numero decimal
+                      contadorToken = contadorToken + 1;
+                  end
+              else --i>len
                   --salva 
-                  print("float2",token)
+                  contadorToken = contadorToken + 1;
+                  print(contadorToken,"float2",token)
                   hmm = false
-                end
-            else
-                --salva erro no numero
-                token = token .. c
-                hmm = false
-                print("não possui apenas numeros2", token)
-                i=len+1
+              end
+          else
+              --salva erro no numero
+              token = token .. c
+              hmm = false
+              contadorToken = contadorToken + 1;
+              print(contadorToken,"não possui apenas numeros2", token)
+              i=len+1
             end
           end--s
           if hmm then
             --salva  numero normal
-            print("numero hmm",token)
+            contadorToken = contadorToken + 1;
+            print(contadorToken,"numero hmm",token)
           end
-      end
+      --end
       i=i+1
     end
   end
@@ -247,5 +297,4 @@ f = io.open(file, "rb")
 for line in io.lines(file) do 
   linhas = linhas + 1; 
   getTokens(line,linhas)
-  a[linhas] = {};
 end
