d3b6fecba201518a630fa937db3e2f9001775932
yendorr <ro.rss.129@gmail.com>
2018-06-07 15:52:57 -0300

    lexico

--- Lexico.lua
+++ Lexico.lua
@@ -11,24 +11,24 @@ c = nil;
 palavra = nil
 
 function analisa(palavra)
-    len = palavra:len()
-    posW = 1
-    c = palavra:sub(posW,posW)
-    while posW <= len do
-        estadoInicial()
-    end
+  len = palavra:len()
+  posW = 1
+  c = palavra:sub(posW,posW)
+  while posW <= len do
+    estadoInicial()
+  end
 end
 
 function anda()
-    posW = posW + 1
-    c = palavra:sub(posW,posW)
-    colunaNoCodigo = colunaNoCodigo + 1
+  posW = posW + 1
+  c = palavra:sub(posW,posW)
+  colunaNoCodigo = colunaNoCodigo + 1
 end
 
 function volta()
-    posW = posW - 1
-    c = palavra:sub(posW,posW)
-    colunaNoCodigo = colunaNoCodigo - 1
+  posW = posW - 1
+  c = palavra:sub(posW,posW)
+  colunaNoCodigo = colunaNoCodigo - 1
 end
 
 function salvaToken(bufferDoToken, tipoDoToken)
@@ -44,324 +44,436 @@ function salvaToken(bufferDoToken, tipoDoToken)
 end
 
 function log()
-    print("palavra - c",palavra,c)
-    print("pos - len",posW , len)
-    print(contadorTokens,token[contadorTokens].texto,"\n")
+  print("palavra - c",palavra,c)
+  print("pos - len",posW , len)
+  print(contadorTokens,token[contadorTokens].texto,token[contadorTokens].tipo,"\n")
+end
+
+function round(x)
+  if x%2 ~= 0.5 then
+    return math.floor(x+0.5)
+  end
+  return x-0.5
+end
+
+function buscaBinaria(buffer,i,j)
+    m = math.floor( (i+j)/2) 
+    if buffer == tabelaPalavras[m] then
+        return true
+    end
+    if i>=j then
+        return false
+    end
+    if buffer < tabelaPalavras[m] then
+        return buscaBinaria(buffer,i,m-1)
+    else
+        return buscaBinaria(buffer,m+1,j)
+    end
+    
 end
 
 function estadoInicial()    
-    if isMaior(c) then
-        estadoMaior()
-    elseif isMenor(c) then
-        estadoMenor()
-    elseif isIgual(c) then
-        estadoIgual()
-    elseif isComercial(c) then
-        estadoOcta()
-    elseif isPorcento(c) then
-        estadoBinario()
-    elseif isSifrao(c) then
-        --estadoHexa()
-    elseif isPV(c) then
-        estadoPV()
-    elseif isDP(c) then
-        estadoDP()
-    elseif isMenos(c) then
-        estadoMenos()
-    elseif isAritmetico(c) then
-        estadoAritimetico()
-    elseif isAlpha(c) then
-        
-    elseif isAspa(c) then
-        estadoAspa()
-    elseif isAspas(c) then
-        estadoAspas()
-    elseif isAChave(c) then
-        estadoAChave()
-    elseif isAParente(c) then
-        estadoAParente()
-    elseif isAColchete(c) then
-        estadoAColchete()
-    elseif isBChave(c) then
-        estadoBChave()
-    elseif isPonto(c) then
-        estadoPonto()
-    elseif isBParente(c) then
-        estadoBParente()
-    elseif isBColchete(c) then
-        estadoBColchete()
-    elseif isDec(c) then
-        estadoNumero()
-    end
+  if isMaior(c) then
+    estadoMaior()
+  elseif isMenor(c) then
+    estadoMenor()
+  elseif isIgual(c) then
+    estadoIgual()
+  elseif isComercial(c) then
+    estadoOcta()
+  elseif isPorcento(c) then
+    estadoBinario()
+  elseif isSifrao(c) then
+    estadoHexa()
+  elseif isPV(c) then
+    estadoPV()
+  elseif isDP(c) then
+    estadoDP()
+  elseif isMenos(c) then
+    estadoMenos()
+  elseif isAritmetico(c) then
+    estadoAritimetico()
+  elseif isAlpha(c) then
+    estadoIdentificador()
+  elseif isAspa(c) then
+    estadoAspa()
+  elseif isAspas(c) then
+    estadoAspas()
+  elseif isAChave(c) then
+    estadoAChave()
+  elseif isAParente(c) then
+    estadoAParente()
+  elseif isAColchete(c) then
+    estadoAColchete()
+  elseif isBChave(c) then
+    estadoBChave()
+  elseif isPonto(c) then
+    estadoPonto()
+  elseif isBParente(c) then
+    estadoBParente()
+  elseif isBColchete(c) then
+    estadoBColchete()
+  elseif isVirgula(c) then
+    estadoVirgula()
+  elseif isDec(c) then
+    estadoNumero()
+  end
 end
 
 function estadoMenor()
-    buffer = c
+  buffer = c
+  anda()
+  if isMaior(c) then 
+    buffer = buffer .. c
+    salvaToken(buffer,"diferente")
+    log()
     anda()
-    if isMaior(c) then 
-        buffer = buffer .. c
-        
-        
-        salvaToken(buffer,"diferente")
-        
-        anda()
-        return 1
-        
-    elseif isIgual(c) then --and i<=len then
-        buffer = buffer .. c
-        
-        salvaToken(buffer,"menorIgual")
-        
-        log()
-        anda()
-        return 1
-    else
-        salvaToken(buffer,"menor")
-        log()
-        return 1
-    end
+    return 1
+
+  elseif isIgual(c) then --and i<=len then
+    buffer = buffer .. c
+    salvaToken(buffer,"menorIgual")
+    log()
+    anda()
+    return 1
+  else
+    salvaToken(buffer,"menor")
+    log()
+    return 1
+  end
 end
 
 function estadoMaior()
-    buffer = c
+  buffer = c
+  anda()
+
+  if isIgual(c) then
+    bueffer = buffer .. c
+    salvaToken(buffer,"maiorIgual")
+    log()
     anda()
-    
-    if isIgual(c) then
-        salvaToken(buffer,"menorIgual")
-        log()
-        anda()
-        return 1
-    else    
-        salvaToken(buffer,"maior")
-        log()
-        return 1;
-    end
+    return 1
+  else    
+    salvaToken(buffer,"maior")
+    log()
+    return 1;
+  end
 end
 
 function estadoIgual()  
-    buffer = c
-    anda()
-    if(isIgual(c)) then
-        buffer = buffer .. c
-        salvaToken(buffer,"igual")
-        log()
-        anda()
-        return 1
-    end
-    salvaToken(buffer,"atribuicao")
+  buffer = c
+  anda()
+  if(isIgual(c)) then
+    buffer = buffer .. c
+    salvaToken(buffer,"igual")
     log()
+    anda()
     return 1
+  end
+  salvaToken(buffer,"atribuicao")
+  log()
+  return 1
 end
 
 function estadoPV()
-    buffer = c
-    anda()
-    salvaToken(buffer,"pontoVirgula")
-    log()
-    return 1
+  buffer = c
+  anda()
+  salvaToken(buffer,"pontoVirgula")
+  log()
+  return 1
 end
 
 function estadoDP()
-    buffer = c
+  buffer = c
+  anda()
+
+  if isIgual(c) then
+    buffer = buffer .. c
+    salvaToken(buffer,"atribuição")
     anda()
-    salvaToken(buffer,"doisPontos")
     log()
     return 1
+  end
+  salvaToken(buffer,"doisPontos")
+  log()
+  return 1
 end
 
 function estadoMenos()
-    buffer = c
-    anda()
-    salvaToken(buffer,"menos")
-    log()
-    return 1
+  buffer = c
+  anda()
+  salvaToken(buffer,"menos")
+  log()
+  return 1
 end
 
 function estadoAritimetico()
-    buffer = c
-    anda()
-    salvaToken(buffer,"aritmetico")
-    log()
-    return 1
+  buffer = c
+  anda()
+  salvaToken(buffer,"aritmetico")
+  log()
+  return 1
 end
 
 function estadoAspa()
-    buffer = c
-    anda()
-    salvaToken(buffer,"aspa")
-    log()
-    return 1
+  buffer = c
+  anda()
+  salvaToken(buffer,"aspa")
+  log()
+  return 1
 end
 
 function estadoAspas()
-    buffer = c
-    anda()
-    salvaToken(buffer,"aspas")
-    log()
-    return 1
+  buffer = c
+  anda()
+  salvaToken(buffer,"aspas")
+  log()
+  return 1
 end
 
 function estadoAChave()
-    buffer = c
-    anda()
-    salvaToken(buffer,"abreChave")
-    log()
-    return 1
+  buffer = c
+  anda()
+  salvaToken(buffer,"abreChave")
+  log()
+  return 1
 end
 function estadoAParente()
-    buffer = c
-    anda()
-    salvaToken(buffer,"abreParente")
-    log()
-    return 1
+  buffer = c
+  anda()
+  salvaToken(buffer,"abreParente")
+  log()
+  return 1
 end
 
 function estadoAColchete()
-    buffer = c
-    anda()
-    salvaToken(buffer,"abreColchete")
-    log()
-    return 1
+  buffer = c
+  anda()
+  salvaToken(buffer,"abreColchete")
+  log()
+  return 1
 end
 
 function estadoBChave()
-    buffer = c
+  buffer = c
+  anda()
+  salvaToken(buffer,"abre colchete")
+  log()
+  return 1
+end
+
+function estadoPonto()
+  buffer = c
+  anda()
+  if(isPonto(c)) then
+    buffer = buffer .. c
+    salvaToken(buffer,"ponto ponto")
     anda()
-    salvaToken(buffer,"abre colchete")
+    log()
+  end
+end
+
+function estadoBColchete()
+  buffer = c
+  anda()
+  salvaToken(buffer,"fecha colchete")
+  log()
+end
+
+function estadoVirgula()
+  buffer = c
+  anda()
+  salvaToken(buffer,"virgula")
+  log()
+end
+
+function estadoBParente()
+  buffer = c
+  anda()
+  salvaToken(buffer,"fecha parente")
+  log()
+end
+
+function estadoBinario()
+  buffer = c
+  anda()
+  while isBin(c) do
+    buffer = buffer .. c
+    anda()
+  end
+  if isColado(c) then
+    salvaToken(buffer,"numero binario")
+    log()
+    return 1;
+  end
+  if(c ~= "") then
+    salvaToken(buffer, "numero binario com erro")
+    anda()
+    log()
+    return 0;
+  else
+    salvaToken(buffer,"numero binario")
+    anda()
+    log()
+    return 1;
+  end
+end
+
+function estadoOcta() 
+  buffer = c
+  anda()
+  while isOcta(c) do
+    buffer = buffer .. c
+    anda()
+  end
+  if(c ~= "") then
+    salvaToken(buffer, "numero octa com erro")
+    anda()
+    log()
+    return 1
+  else
+    salvaToken(buffer,"numero octa")
+    anda()
+    log() 
+    return 0
+  end
+end
+
+function estadoHexa()
+  buffer = c
+  anda()
+  while isHexa(c) do
+    buffer = buffer .. c
+    anda()
+  end
+  if(c ~= "") then
+    salvaToken(buffer,"numero hexa com erro")
+    anda()
+    log()
+    return 1
+  else
+    salvaToken(buffer,"numero hexa")
+    anda()
+    log()
+    return 0;
+  end
+end
+
+function estadoNumero()
+  buffer = c
+  anda()
+  while isDec(c) do
+    buffer = buffer .. c
+    anda()
+  end
+  if isPonto(c) then
+    anda()
+    if(isPonto(c)) then
+      salvaToken(buffer,"numero")
+      log()
+      salvaToken("..","ponto ponto")
+      log()
+      anda()
+      return 2
+    end
+    if isDec(c) then
+      volta()
+      buffer = buffer .. c
+      anda()
+      while isDec(c) do
+        buffer = buffer .. c
+        anda()
+      end  
+      if isColado(c) then
+        salvaToken(buffer,"float")
+        log()
+        return 1
+      end
+      if(c ~= "") then
+        salvaToken(buffer,"float com erro")
+        anda()
+        log()
+        return 1
+      else
+        salvaToken(buffer,"float")
+        anda()
+        log()
+        return 1
+      end
+    end
+  end
+  if isColado(c) then
+    salvaToken(buffer,"numero")
+    log()
+    return 1;
+  end
+
+  if (c ~= "") then
+    salvaToken(buffer,"numero com erro")
+    anda()
+    log()
+    return 0;
+  else
+    salvaToken(buffer,"numero")
+    anda()
+    log()
+    return 1;
+  end
+
+end
+
+function estadoIdentificador()
+  buffer = c
+  anda()
+  while isAlpha(c) or isDec(c) do--r isUnderLine() do
+    buffer = buffer .. c
+    anda()
+  end
+  if isColado(c) then
+    if buscaBinaria(buffer,1,table.getn(tabelaPalavras)) then
+        salvaToken(buffer,buffer) 
+    else
+        salvaToken(buffer,"identificador") 
+    end
+    log()
+    return 1
+  end
+  if(c ~= "") then
+    salvaToken(buffer,"identificador com erro")
+    anda()
+    log()
+    return 0
+  end
+    
+    if buscaBinaria(buffer,1,table.getn(tabelaPalavras)) then
+        salvaToken(buffer,buffer) 
+    else
+        salvaToken(buffer,"identificador") 
+    end
     log()
     return 1
 end
 
-function estadoPonto()
-    buffer = c
-    anda()
-    if(isPonto(c)) then
-        buffer = buffer .. c
-        salvaToken(buffer,"ponto ponto")
-        anda()
-        log()
-    end
-end
-        
-function estadoBParente()
-    buffer = c
-    anda()
-    salvaToken(buffer,"fecha parente")
-    log()
-end
-
-function estadoBinario()
-    buffer = c
-    anda()
-    while isOcta(c) do
-        buffer = buffer .. c
-        anda()
-    end
-    if(c) then
-        salvaToken(buffer, "numero octal")
-    else
-        salvaToken(buffer,"numero octal com erro")
-    end
-    anda()
-    log()
-end
-
-function estadoOcta() 
-    buffer = c
-    anda()
-    while isOcta(c) do
-        buffer = buffer .. c
-        anda()
-    end
-    if(c) then
-        salvaToken(buffer, "numero binario")
-        anda()
-        log()
-        return 1
-    else
-        salvaToken(buffer,"numero binario com erro")
-        anda()
-        log() 
-        return 0
-    end
-end
-
-function estadoHexa()
-    buffer = c
-    anda()
-    while isHexa(c) do
-    end
-end
-
-function estadoNumero()
-    buffer = c
-    anda()
-    while isDec(c) do
-        buffer = buffer .. c
-        anda()
-    end
-    if isPonto(c) then
-        anda()
-        if(isPonto(c)) then
-            salvaToken(buffer,"numero")
-            log()
-            salvaToken("..","ponto ponto")
-            log()
-            anda()
-            return 2
-        end
-        if isDec(c) then
-            volta()
-            buffer = buffer .. c
-            anda()
-            while isDec(c) do
-                buffer = buffer .. c
-                anda()
-            end  
-            if isColado(c) then
-                salvaToken(buffer,"float")
-                log()
-                return 1
-            end
-            salvaToken(buffer,"float com erro")
-            log()
-        end
-    end
-    if isColado(c) then
-        salvaToken(buffer,"numero")
-        log()
-        return 1;
-    end
-    salvaToken(buffer,"numero com erro")
-    log()
-    return 0;
-end
-
-function estadoBColchete()
-    buffer = c
-    anda()
-    salvaToken(buffer,"fecha colchete")
-    log()
-end
-
 function main()
-    leArquivo()
-    for line in io.lines(file) do
-        linhaNoCodigo = linhaNoCodigo + 1
-        colunaNoCodigo = 1
-        for word in string.gmatch(line, '[^ \n]+') do
-            palavra = word
-            analisa (palavra)
-        end
+  leArquivo()
+  for line in io.lines(file) do
+    linhaNoCodigo = linhaNoCodigo + 1
+    colunaNoCodigo = 1
+    for word in string.gmatch(line, '[^ \n]+') do
+      palavra = word
+      analisa (palavra)
     end
+  end
 end
 
 function leArquivo()
-    file = 'pascal.txt'
-    f = io.open(file, "rb")
-    if f == nil then 
-        print("deu ruim")
-    end
+  file = 'pascal.txt'
+  f = io.open(file, "rb")
+  if f == nil then 
+    print("deu ruim")
+  end
 end
+
+table.sort(tabelaPalavras, function(a,b) return a<b end)
 
 main()
--- header.lua
+++ header.lua
@@ -1,6 +1,8 @@
+tabelaPalavras ={"and", "array", "asm", "begin", "break", "case", "const", "constructor", "continue", "destructor", "div", "do", "downto","else", "end", "false", "file", "for", "function", "goto", "if", "implementation", "in", "inline", "interface", "label","mod", "nil", "not" , "object", "of", "on", "operator", "or" ,"packed", "procedure", "program", "record", "repeat", "set","shl", "shr", "string", "then", "to", "true", "type", "unit", "until", "uses", "var", "while", "with", "xor", "absolute","abstract", "alias", "assembler", "cdecl", "cppdecl", "default", "export", "external", "forward", "generic", "index","local", "name", "nostackframe", "oldfpccall", "override", "pascal", "private", "protected", "public", "published", "read","readln", "register", "reintroduce", "reset", "rewrite", "safecall", "softfloat", "specialize", "stdcall", "virtual","write", "writeln", "as", "class", "dispinterface", "except", "exports", "finalization", "finally", "initialization","inline", "is", "library", "on", "out", "property", "raise", "resourcestring", "threadvar", "try","byte","shortint","smallinte","word","integer","word","cardinal","longint","longword","int64","qword"}
+
 function isAlpha(char)
-  asc = string.byte(char)
-  return (asc >= 65 and asc <=90) or (asc>=97 and asc<=122)
+    asc = string.byte(char)
+    return asc == 65 or asc == 66 or asc == 67 or asc == 68 or asc == 69 or asc == 70 or asc == 71 or asc == 72 or asc == 73 or    asc == 74 or asc == 75 or asc == 76 or asc == 77 or asc == 78 or asc == 79 or asc == 80 or asc == 81 or asc == 82 or asc == 83 or asc == 84 or asc == 85 or asc == 86 or asc == 87 or asc == 88 or asc == 89 or asc == 90 or asc == 91 or asc == 92 or asc == 93 or asc == 94 or asc == 95 or asc == 97 or asc == 98 or asc == 99 or asc == 100 or asc == 101 or asc == 102 or asc == 103 or          asc == 104 or asc == 105 or asc == 106 or asc == 107 or asc == 108 or asc == 109 or asc == 110 or asc == 111 or asc == 112 or     asc == 113 or asc == 114 or asc == 115 or asc == 116 or asc == 117 or asc == 118 or asc == 119 or asc == 120 or asc == 121 or     asc == 122
 end
 
 function isBin(char)
@@ -23,7 +25,7 @@ end
 
 function isHexa(char)
         asc = string.byte(char)
-        return (asc == 48 or asc == 49 or asc == 50 or asc == 51 or asc == 52  or asc == 53 or asc == 54 or asc == 55 or asc == 56 or asc == 57 or asc == 65 or asc == 66 or asc == 67 or asc == 68 or asc == 69 or asc == 70 or asc == 97 or asc == 98 or asc == 99 or asc == 100  or asc == 11 or asc == 102)
+        return (asc == 48 or asc == 49 or asc == 50 or asc == 51 or asc == 52  or asc == 53 or asc == 54 or asc == 55 or asc == 56 or asc == 57 or asc == 65 or asc == 66 or asc == 67 or asc == 68 or asc == 69 or asc == 70 or asc == 97 or asc == 98 or asc == 99 or asc == 100  or asc == 101 or asc == 102)
 end
 
 function isAritmetico(char)
@@ -122,7 +124,9 @@ function isBColchete(char)
 end
 
 function isComercial(char)
+    if(char == nil) then return false else
     asc = string.byte(char)
+    end
     return asc == 38
 end
 
@@ -136,5 +140,5 @@ function identificador(char)
 end  
 
 function isColado(char)
-  return isAritmetico(char) or isMenos(char) or isMenor(char) or isMaior(char) or isIgual(char) or isPV(char) or isDP(char) or isVirgula(char) or isAParente(char)
+  return isAritmetico(char) or isMenos(char) or isMenor(char) or isMaior(char) or isIgual(char) or isPV(char) or isDP(char) or isVirgula(char) or isAParente(char) or c==nil
 end  
